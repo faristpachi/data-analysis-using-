4e9879b5c95a5ca2b796b8abf0bd4fdfc1c022bf
faristpachi <faristpachi@gmail.com>
2021-05-19 06:38:40 +0000

    till qplot date fixed

--- covid19data_analysis.R
+++ covid19data_analysis.R
@@ -10,4 +10,16 @@ tsc
 
 tsc= data.frame(t(tsc))
 tsc
+tsc= cbind(rownames(tsc), data.frame(tsc, row.names=NULL))
+colnames(tsc)=c('Date','Confirmed')
+tsc=tsc[-c(1:4),]
+tsc
 
+summary(tsc)
+tsc$Date=ymd(tsc$Date)
+summary(tsc)
+tsc$Confirmed=as.numeric(tsc$Confirmed)
+summary(tsc)
+qplot(Date,Confirmed,data = tsc,main='Covid 19 Confirmed Cases in USA')
+
+qplot(Date,Confirmed,data = tsc,main='Covid 19 Confirmed Cases in USA')
